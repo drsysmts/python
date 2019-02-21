e616c756f6f84bed111fa59e4a9d8f6ebbe0020f
drsysmts <30734741+drsysmts@users.noreply.github.com>
2019-02-21 23:56:55 +0100

    Merge branch 'master' of https://github.com/drsysmts/python
    # Conflicts:
    #	lesson1/Home/github1.py

--- lesson1/Home/github1.py
+++ lesson1/Home/github1.py
@@ -7,3 +7,14 @@ print(sum,mul,sus)
 #code work good
 caution={1:"name",2:"age"}  #dic
 print("local333")
+
+text = "abc abc                   "
+text = text.lower()
+counter = {}
+for ch in text:
+    if ch not in counter:
+        counter[ch] = 1
+    else:
+        counter[ch] += 1
+
+print(counter.items())
